4ea2812c6f521edc011a5345fc5b39358ca56b81
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-02 17:09:20 +0200

    init microservice project

---
+++ src/main/java/it/gov/pagopa/logextractor/PnLogextractorBeApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.logextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnLogextractorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnLogextractorBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/logextractor/PnLogextractorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.logextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnLogextractorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
