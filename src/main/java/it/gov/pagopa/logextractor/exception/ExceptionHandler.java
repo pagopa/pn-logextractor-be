015acbd86d06c871e28aa413b200209e8c7d1f14
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-04 10:10:02 +0200

    added dependencies and scripts for aws integration, setup exception handler

---
+++ src/main/java/it/gov/pagopa/logextractor/exception/ExceptionHandler.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.logextractor.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
