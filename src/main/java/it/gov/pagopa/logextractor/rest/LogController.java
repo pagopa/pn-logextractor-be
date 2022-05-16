12c1debd507ee9fb5d3999076f69674d0b5520c8
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-16 10:46:58 +0200

    Changed the setup of person controller method

--- src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
+++ src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.logextractor.rest;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
