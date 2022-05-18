b310fc07035b197820c65af63fe5e88911f4c2b0
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-05-18 17:06:32 +0300

    Added no argument constructor

--- src/main/java/it/gov/pagopa/logextractor/dto/response/EnsureRecipientByExternalIdResponseDto.java
+++ src/main/java/it/gov/pagopa/logextractor/dto/response/EnsureRecipientByExternalIdResponseDto.java
@@ -3,9 +3,11 @@ package it.gov.pagopa.logextractor.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class EnsureRecipientByExternalIdResponseDto {
 
--- src/main/java/it/gov/pagopa/logextractor/dto/response/GetRecipientDenominationByInternalIdResponseDto.java
+++ src/main/java/it/gov/pagopa/logextractor/dto/response/GetRecipientDenominationByInternalIdResponseDto.java
@@ -8,23 +8,27 @@ import it.gov.pagopa.logextractor.util.Constants;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class GetRecipientDenominationByInternalIdResponseDto {
 
 	@Size(min = 16, max = 16)
 	@Pattern(regexp = Constants.FISCAL_CODE_PATTERN, message = "Invalid Tax ID")
 	String taxId;
-	
+
 	@Size(min = 1, max = 100, message = "Invalid Intenal ID")
 	@Pattern(regexp = Constants.INTERNAL_ID_PATTERN)
 	String internalId;
-	
+
 	@RecipientType
 	String recipientType;
-	
+
 	@Pattern(regexp = Constants.ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN, message = "Invalid denomination")
 	String denomination;
 }
