ae5331d4f8ce58ee1c4ef06beea156e8a42a9da9
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-05-18 17:08:49 +0300

    Added functionality to retrieve person's basic data

--- src/main/java/it/gov/pagopa/logextractor/rest/PersonController.java
+++ src/main/java/it/gov/pagopa/logextractor/rest/PersonController.java
@@ -1,22 +1,54 @@
 package it.gov.pagopa.logextractor.rest;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.gov.pagopa.logextractor.annotation.RecipientType;
 import it.gov.pagopa.logextractor.dto.response.GetBasicDataResponseDto;
+import it.gov.pagopa.logextractor.service.PersonService;
+import it.gov.pagopa.logextractor.util.Constants;
 
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
-	
+
+	@Autowired
+	PersonService personService;
+
+	/**
+	 * A controller method used to retrieve the basic data of a person (the basic
+	 * data would be either unique identifier or tax code)
+	 * 
+	 * @param extractionType required parameter, which represents the integer value
+	 *                       of the extraction type
+	 * @param recipientType  required parameter, which represents the two values of
+	 *                       the enum {@link RecipientType}
+	 * @param ticketNumber   the ticket number of a person, this parameter should be
+	 *                       passed if the unique identifier should be retrieved as
+	 *                       basic data
+	 * @param taxId          the tax id of a person, this parameter should be passed
+	 *                       if the unique identifier should be retrieved as basic
+	 *                       data
+	 * @param personId       the unique identifier of a person, this parameter
+	 *                       should be passed if the tax code should be retrieved as
+	 *                       basic data
+	 * @return basic data of a person, depending on which parameters are present
+	 */
 	@GetMapping("/basicData")
-	public ResponseEntity<GetBasicDataResponseDto> getBasicData(@RequestParam(required = true) String extractionType,
-																@RequestParam(required = false) int ticketNumber,
-																@RequestHeader(name = "fiscal-code", required = false) String taxId,
-																@RequestHeader(name = "person-id", required = false) String personId){
-		return ResponseEntity.ok(null);
+	public ResponseEntity<GetBasicDataResponseDto> getBasicData(@RequestParam(required = true) Integer extractionType,
+			@RequestParam(required = true) Boolean recipientType,
+			@RequestParam(required = false) @Pattern(regexp = Constants.ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN, message = "Invalid ticket number") String ticketNumber,
+			@RequestHeader(name = "fiscal-code", required = false) @Size(min = 16, max = 16) @Pattern(regexp = Constants.FISCAL_CODE_PATTERN, message = "Invalid Tax ID") String taxId,
+			@RequestHeader(name = "person-id", required = false) @Size(min = 1, max = 100, message = "Invalid person id") @Pattern(regexp = Constants.INTERNAL_ID_PATTERN) String personId) {
+		return ResponseEntity
+				.ok(personService.getPersonsBasicData(extractionType, recipientType, ticketNumber, taxId, personId));
 	}
 }
--- src/main/java/it/gov/pagopa/logextractor/service/PersonService.java
+++ src/main/java/it/gov/pagopa/logextractor/service/PersonService.java
@@ -1,5 +1,32 @@
 package it.gov.pagopa.logextractor.service;
 
+import org.springframework.web.client.HttpServerErrorException;
+
+import it.gov.pagopa.logextractor.annotation.RecipientType;
+import it.gov.pagopa.logextractor.dto.response.GetBasicDataResponseDto;
+
+/**
+ * An interface containing all methods for persons
+ */
 public interface PersonService {
 
+	/**
+	 * Service method that makes a request to Piattaforma Notifiche external service
+	 * and retrieves basic data about a person. If ticketNumber and taxId are
+	 * present, the method retrieves a unique identifier, which is the encrypted
+	 * fiscal code of a person. If personId is present, the method retrieves the tax
+	 * code of a person.
+	 * 
+	 * @param extractionType required parameter, which represents the integer value
+	 *                       of the extraction type
+	 * @param recipientType  required parameter, which represents the two values of
+	 *                       the enum {@link RecipientType}
+	 * @param ticketNumber   the ticket number of a person
+	 * @param taxId          the tax id of a person
+	 * @param personId       the unique identifier of a person
+	 * @return basic data for a person, depending on which parameters are present
+	 * @throws HttpServerErrorException
+	 */
+	GetBasicDataResponseDto getPersonsBasicData(Integer extractionType, Boolean recipientType, String ticketNumber,
+			String taxId, String personId) throws HttpServerErrorException;
 }
