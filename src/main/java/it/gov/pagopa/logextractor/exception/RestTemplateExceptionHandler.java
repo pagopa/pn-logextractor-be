3fccf22de27e1891dbc283250ec2572bfb93a2b6
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-05-18 17:03:09 +0300

    Uncommented throwing of IO exceptions

--- src/main/java/it/gov/pagopa/logextractor/exception/RestTemplateExceptionHandler.java
+++ src/main/java/it/gov/pagopa/logextractor/exception/RestTemplateExceptionHandler.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatus.Series;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
+
+import it.gov.pagopa.logextractor.dto.response.ApiError;
 
 public class RestTemplateExceptionHandler implements ResponseErrorHandler {
 
@@ -19,10 +22,10 @@ public class RestTemplateExceptionHandler implements ResponseErrorHandler {
     public void handleError(ClientHttpResponse httpResponse) throws IOException {
         if (httpResponse.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR) {
         	System.out.println("Errore 500");
-//        	throw new IOException("Errore 500");
+        	throw new IOException("Errore 500");
         } else if (httpResponse.getStatusCode().series() == HttpStatus.Series.CLIENT_ERROR) {
         	System.out.println("Errore 400");
-//        	throw new IOException("Errore 400");
+        	throw new IOException("Errore 400");
         }
     }
 }
