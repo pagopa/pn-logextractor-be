2ad31aebe32006c2368e08048d84f7db1adf3ed9
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-20 12:10:27 +0200

    Changed CloudFormation templates after review

--- src/main/java/it/gov/pagopa/logextractor/exception/RestExceptionHandler.java
+++ src/main/java/it/gov/pagopa/logextractor/exception/RestExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
