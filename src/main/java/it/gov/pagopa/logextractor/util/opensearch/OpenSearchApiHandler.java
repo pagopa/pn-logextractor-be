6a09dd84dfe9c21e1b2f269004443e6aab1bd72e
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-25 17:26:19 +0200

    added basic auth header per opensearch integration, added application properties file for local tests

--- src/main/java/it/gov/pagopa/logextractor/util/opensearch/OpenSearchApiHandler.java
+++ src/main/java/it/gov/pagopa/logextractor/util/opensearch/OpenSearchApiHandler.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.logextractor.util.opensearch;
 
 import java.util.ArrayList;
+//import java.util.Base64;
 import java.util.List;
 
 import org.springframework.http.HttpEntity;
@@ -16,11 +17,16 @@ import it.gov.pagopa.logextractor.config.ApplicationContextProvider;
  * */
 public class OpenSearchApiHandler {
 
-	public void getDocuments(ArrayList<OpenSearchQuerydata> queryData, boolean isBooleanQuery, String url) {
+	//TODO: Add return type to get documents after performing query
+	public void getDocuments(ArrayList<OpenSearchQuerydata> queryData, boolean isBooleanQuery, String url, 
+								String basicAuthUsername, String basicAuthPassword) {
 		OpenSearchQueryConstructor queryConstructor = new OpenSearchQueryConstructor();
+//		String authStr = "admin:admin";
+//	    String base64Creds = Base64.getEncoder().encodeToString(authStr.getBytes());
 		RestTemplate client = (RestTemplate) ApplicationContextProvider.getBean("restTemplate");
 		HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+        requestHeaders.setBasicAuth(basicAuthUsername, basicAuthPassword);// add("Authorization", "Basic " + base64Creds);
         List<MediaType> acceptedTypes = new ArrayList<MediaType>();
         acceptedTypes.add(MediaType.APPLICATION_JSON);
         requestHeaders.setAccept(acceptedTypes);
