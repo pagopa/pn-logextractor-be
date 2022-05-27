425351bb4af714702e6d572725f0e5151bf5fe5e
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-27 09:53:37 +0200

    CloudFormation template adjustment, added response dto class for pn external service

---
+++ src/main/java/it/gov/pagopa/logextractor/dto/response/LegalFactDownloadMetadataResponseDto.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.logextractor.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class LegalFactDownloadMetadataResponseDto {
+
+	private String filename;
+	private Integer contentLength;
+	private String url;
+	private Integer retryAfter;
+}
