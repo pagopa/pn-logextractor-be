b4e82f9adda302b783821a6c60c042e3bafa580f
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-27 17:40:54 +0200

    added disabling of ssl certificate verification for local tests purposes only

--- src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
+++ src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
@@ -1,5 +1,9 @@
 package it.gov.pagopa.logextractor.config;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +13,15 @@ public class BeanConfiguration {
 
 	@Bean
 	public RestTemplate restTemplate() {
+		//TODO: START -  to delete when deploying in dev environment, this is just for local test purposes
+		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+			
+			@Override
+		    public boolean verify(String hostname, SSLSession session) {
+		        return true;
+		    }
+		});
+		//END
 		return new RestTemplate();
 	}
 }
