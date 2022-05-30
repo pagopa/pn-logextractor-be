b53e4876493337ae894963f525ce154364a54dd0
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-30 17:04:18 +0200

    added base opensearch response dto

---
+++ src/main/java/it/gov/pagopa/logextractor/dto/response/baseOpenSearchResponseDto.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.logextractor.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class baseOpenSearchResponseDto {
+
+	private String timestamp;
+	private String version;
+	private String message;
+	private String logger_name;
+	private String thread_name;
+	private String level;
+	private Integer level_value;
+	private String trace_id;
+}
