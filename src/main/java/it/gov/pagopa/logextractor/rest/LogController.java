68137ba6e6f9818a888b16d4111b2376f3afd9b6
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-05-30 16:26:03 +0200

    endpoint input changes

--- src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
+++ src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.logextractor.rest;
 
+import javax.validation.constraints.Pattern;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import it.gov.pagopa.logextractor.dto.response.DownloadLogResponseDto;
+import it.gov.pagopa.logextractor.util.Constants;
 
 @RestController
 @RequestMapping("/logextractor/v1/logs")
@@ -14,26 +17,30 @@ public class LogController {
 
 	@GetMapping(value = "/persons", produces="application/zip")
 	public ResponseEntity<DownloadLogResponseDto> getPersonActivityLogs(@RequestParam(required = true) int ticketNumber,
-																		@RequestParam(required = false) Integer iun, 
-																		@RequestParam(required = false) Integer months, 
-																		@RequestParam(required = true) boolean deanonimization,
-																		@RequestParam(required = false) String taxId, 
-																		@RequestParam(required = false) String personId){
+												@RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String referenceDate,
+												@RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String dateFrom,
+												@RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String dateTo,
+												@RequestParam(required = false) Integer iun, 
+												@RequestParam(required = false) Integer months, 
+												@RequestParam(required = true) boolean deanonimization,
+												@RequestParam(required = false) String taxId, 
+												@RequestParam(required = false) String personId){
 		return ResponseEntity.ok(null);
 	}
 	
 	@GetMapping(value = "/operators", produces="application/zip")
 	public ResponseEntity<DownloadLogResponseDto> getOperatorsActivityLogs(@RequestParam(required = true) int ticketNumber, 
-																		   @RequestParam(required = true) int months, 
-																		   @RequestParam(required = false) String taxId) {
+											   @RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String dateFrom ,
+											   @RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String dateTo,
+											   @RequestParam(required = false) String taxId) {
 		return ResponseEntity.ok(null);
 	}
 	
 	@GetMapping(value = "/notifications", produces="application/zip")
 	public ResponseEntity<DownloadLogResponseDto> getNotificationLogs(@RequestParam(required = true) int ticketNumber,
-																	  @RequestParam(required = false) Integer iun,
-																	  @RequestParam(required = false) Integer referenceMonth,
-																	  @RequestParam(required = false) String ipaCode){
+											  @RequestParam(required = false) Integer iun,
+											  @RequestParam(required = false) @Pattern(regexp = Constants.INPUT_DATE_FORMAT) String referenceMonth,
+											  @RequestParam(required = false) String ipaCode){
 		return ResponseEntity.ok(null);
 	}
 }
--- src/main/java/it/gov/pagopa/logextractor/util/Constants.java
+++ src/main/java/it/gov/pagopa/logextractor/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	public static final String FISCAL_CODE_PATTERN = "^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})|([0-9]{11})$";
 	public static final String INTERNAL_ID_PATTERN = "[A-Za-z0-9~-_]*";
 	public static final String ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN = "^[\\w(?:à,ò,è,é)]+$";
+	public static final String INPUT_DATE_FORMAT = "/([0-9]{4})-(?:[0-9]{2})-([0-9]{2})/";
 }
