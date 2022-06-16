169aaa75630a2a9def0dd0e8d9b8c4cb1f956003
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-06-16 13:39:36 +0300

    Added regex pattern for iun validation

--- src/main/java/it/gov/pagopa/logextractor/util/Constants.java
+++ src/main/java/it/gov/pagopa/logextractor/util/Constants.java
@@ -5,8 +5,9 @@ public class Constants {
 	public static final String FISCAL_CODE_PATTERN = "^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})|([0-9]{11})$";
 	public static final String INTERNAL_ID_PATTERN = "[A-Za-z0-9~-_]*";
 	public static final String ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN = "^[a-zA-Z0-9-]+$";
-	public static final String INPUT_DATE_FORMAT = "/([0-9]{4})-(?:[0-9]{2})-([0-9]{2})/";
-	public static final String INPUT_MONTH_FORMAT = "/([0-9]{4})-(?:[0-9]{2})/";
+	public static final String IUN_PATTERN = "([A-Za-z]{4})-([A-Za-z]{4})-([A-Za-z]{4})-([0-9]{6})-([A-Za-z]{1})-([0-9]{1})";
+	public static final String INPUT_DATE_FORMAT = "([0-9]{4})-(?:[0-9]{2})-([0-9]{2})";
+	public static final String INPUT_MONTH_FORMAT = "([0-9]{4})-(?:[0-9]{2})";
 	public static final String PASSWORD_SPECIAL_CHARS = "!@#$%^&()_+";
 	public static final String ZIP_ARCHIVE_NAME = "export";
 	public static final String FILE_NAME = "dati";
