2489cdcef7eedb1795e571e59319053d7a23534d
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-06-21 14:28:50 +0300

    Added method that retrieves legal fact id from response query

--- src/main/java/it/gov/pagopa/logextractor/util/external/pnservices/NotificationApiHandler.java
+++ src/main/java/it/gov/pagopa/logextractor/util/external/pnservices/NotificationApiHandler.java
@@ -54,6 +54,25 @@ public class NotificationApiHandler {
 	}
 	
 	/**
+	 * Performs a GET HTTP request to the PN external service to retrieve a
+	 * notification legal fact id
+	 * 
+	 * @param url the PN external service base URL
+	 * @param iun the notification iun
+	 * @return the notification legal fact id
+	 */
+	public String getNotificationLegalFactId(String url, String iun) {
+		RestTemplate client = (RestTemplate) ApplicationContextProvider.getBean("simpleRestTemplate");
+		HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+        List<MediaType> acceptedTypes = new ArrayList<MediaType>();
+        acceptedTypes.add(MediaType.APPLICATION_JSON);
+        requestHeaders.setAccept(acceptedTypes);
+        var response = client.getForEntity(url+"/"+iun, String.class);
+        return getLegalFactId(response.getBody());
+	}
+	
+	/**
 	 * Gets the general data of a notification
 	 * @param notificationResponse The PN external service response containing the notification general data
 	 * @return A list containing all the notifications' general data
@@ -93,5 +112,28 @@ public class NotificationApiHandler {
         	}
         }
         return legalStartDate;
+	}
+	
+	/**
+	 * Method that get the legal fact id of a notification
+	 * 
+	 * @param notificationInfo The PN external service response containing the
+	 *                         notification details
+	 * @return the notification legal fact id
+	 */
+	private String getLegalFactId(String notificationInfo) {
+		String legalFactId = null;
+		JSONArray timelineObjectsArray = new JSONObject(notificationInfo).getJSONArray("timeline");
+		for (int index = 0; index < timelineObjectsArray.length(); index++) {
+			JSONObject timelineObject = timelineObjectsArray.getJSONObject(index);
+			if ("REQUEST_ACCEPTED".equalsIgnoreCase(timelineObject.getString("category"))) {
+				JSONArray legalFactIdsArray = timelineObjectsArray.getJSONObject(index).getJSONArray("legalFactsIds");
+				for (int indexFactsIds = 0; indexFactsIds < legalFactIdsArray.length(); indexFactsIds++) {
+					JSONObject legalFactsObject = legalFactIdsArray.getJSONObject(indexFactsIds);
+					legalFactId = legalFactsObject.getString("key");
+				}
+			}
+		}
+		return legalFactId;
 	}
 }
