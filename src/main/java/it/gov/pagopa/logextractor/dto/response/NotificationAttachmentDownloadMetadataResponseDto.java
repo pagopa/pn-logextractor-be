ca90a8877ddb0e7df1e2d736cfd6b7ae143827a5
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-06-27 13:27:28 +0300

    Added no arguments constructor

--- src/main/java/it/gov/pagopa/logextractor/dto/response/NotificationAttachmentDownloadMetadataResponseDto.java
+++ src/main/java/it/gov/pagopa/logextractor/dto/response/NotificationAttachmentDownloadMetadataResponseDto.java
@@ -3,11 +3,13 @@ package it.gov.pagopa.logextractor.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class NotificationAttachmentDownloadMetadataResponseDto {
 	private String filename;
