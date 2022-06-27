90617da92fa134983f68a754c2cbee023d4b0e08
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2022-06-27 13:26:57 +0300

    Added payment filename and pdf extension

--- src/main/java/it/gov/pagopa/logextractor/util/Constants.java
+++ src/main/java/it/gov/pagopa/logextractor/util/Constants.java
@@ -12,8 +12,10 @@ public class Constants {
 	public static final String ZIP_ARCHIVE_NAME = "export";
 	public static final String LEGAL_FACT_FILE_NAME="legalFact";
 	public static final String NOTIFICAION_DOCUMENT_FILE_NAME="notificationDoc";
+	public static final String PAYMENT_DOCUMENT_FILE_NAME="payment";
 	public static final String LOG_FILE_NAME = "dati";
 	public static final String TXT_EXTENSION = ".txt";
+	public static final String PDF_EXTENSION = ".pdf";
 	public static final String CSV_EXTENSION = ".csv";
 	public static final String ZIP_EXTENSION = ".zip";
 	public static final String EXPORT_FOLDER = "export/";
