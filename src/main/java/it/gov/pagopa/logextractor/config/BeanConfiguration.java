7a478bcd063234e0388995eaff4ff6e193c47dc6
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-10-26 16:11:53 +0200

    bean config fix

--- src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
+++ src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
+
 /**
  * Configuration class for defining the custom context beans
  * */
@@ -19,27 +16,11 @@ public class BeanConfiguration {
 
 	@Bean
 	public RestTemplate openSearchRestTemplate() {
-		//TODO: START -  to delete when deploying in dev environment, this is just for local test purposes
-		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-
-			@Override
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		});
 		return new RestTemplate(new SimpleClientHttpRequestWithGetBodyFactory());
 	}
 	
 	@Bean
 	public RestTemplate simpleRestTemplate() {
-		//TODO: START -  to delete when deploying in dev environment, this is just for local test purposes
-		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-
-			@Override
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		});
 		return new RestTemplate();
 	}
 
