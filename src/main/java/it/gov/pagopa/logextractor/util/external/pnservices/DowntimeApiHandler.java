ba1914b64e94eadb6b046d6e141f52d46ffc6206
angelaantonia.cea <angelaantonia.cea@dxc.com>
2022-10-28 12:45:58 +0200

    fixed request fields and added configuration class of offsetdatetime

--- src/main/java/it/gov/pagopa/logextractor/util/external/pnservices/DowntimeApiHandler.java
+++ src/main/java/it/gov/pagopa/logextractor/util/external/pnservices/DowntimeApiHandler.java
@@ -1,21 +1,23 @@
 package it.gov.pagopa.logextractor.util.external.pnservices;
 
-import it.gov.pagopa.logextractor.exception.LogExtractorException;
-import it.gov.pagopa.logextractor.pn_logextractor_be.model.PnStatusResponseDto;
-import it.gov.pagopa.logextractor.pn_logextractor_be.model.PnStatusUpdateEventRequestDto;
-import it.gov.pagopa.logextractor.util.constant.CognitoConstants;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import java.util.ArrayList;
-import java.util.List;
+
+import it.gov.pagopa.logextractor.exception.LogExtractorException;
+import it.gov.pagopa.logextractor.pn_logextractor_be.model.PnStatusResponseDto;
+import it.gov.pagopa.logextractor.pn_logextractor_be.model.PnStatusUpdateEventRequestDto;
+import it.gov.pagopa.logextractor.util.constant.CognitoConstants;
 
 @Component
 public class DowntimeApiHandler {
@@ -39,13 +41,16 @@ public class DowntimeApiHandler {
     }
 
     public void addStatusChangeEvent(List<PnStatusUpdateEventRequestDto> pnStatusUpdateEventRequestDto) {
+    	for(PnStatusUpdateEventRequestDto pnStatusUpdate : pnStatusUpdateEventRequestDto) {
+    		pnStatusUpdate.setSource(MDC.get(CognitoConstants.USER_IDENTIFIER_PLACEHOLDER));
+    	}
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
         requestHeaders.set("x-pagopa-pn-uid", MDC.get(CognitoConstants.USER_IDENTIFIER_PLACEHOLDER));
         List<MediaType> acceptedTypes = new ArrayList<>();
-        acceptedTypes.add(MediaType.APPLICATION_JSON);
+        acceptedTypes.add(MediaType.APPLICATION_PROBLEM_JSON);
         requestHeaders.setAccept(acceptedTypes);
-        HttpEntity<?> entity = new HttpEntity<>(pnStatusUpdateEventRequestDto.toString(), requestHeaders);
+        HttpEntity<?> entity = new HttpEntity<>(pnStatusUpdateEventRequestDto, requestHeaders);
         client.postForEntity(downtimeEventsURL, entity, Void.class);
     }
 }
