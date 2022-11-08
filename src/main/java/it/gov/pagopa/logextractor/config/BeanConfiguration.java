38d8e8c678f0f407131f6537fffbb21dc0f5dfc4
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-11-08 18:58:18 +0100

    removed disabled ssl checks

--- src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
+++ src/main/java/it/gov/pagopa/logextractor/config/BeanConfiguration.java
@@ -7,9 +7,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
+
 /**
  * Configuration class for defining the custom context beans
  * */
@@ -18,27 +16,11 @@ public class BeanConfiguration {
 
 	@Bean
 	public RestTemplate openSearchRestTemplate() {
-		//TODO: START -  to delete when deploying in dev environment, this is just for local test purposes
-		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-
-			@Override
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		});
 		return new RestTemplate(new SimpleClientHttpRequestWithGetBodyFactory());
 	}
 	
 	@Bean
 	public RestTemplate simpleRestTemplate() {
-		//TODO: START -  to delete when deploying in dev environment, this is just for local test purposes
-		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-
-			@Override
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		});
 		return new RestTemplate();
 	}
 
