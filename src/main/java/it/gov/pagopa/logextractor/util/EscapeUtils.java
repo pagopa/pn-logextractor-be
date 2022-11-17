622c6a7654d7d78c21227634ad32cefc3d66bcfe
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-11-17 18:53:16 +0100

    fixed csv value escaping

--- src/main/java/it/gov/pagopa/logextractor/util/EscapeUtils.java
+++ src/main/java/it/gov/pagopa/logextractor/util/EscapeUtils.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.logextractor.util;
 
-import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.text.StringEscapeUtils;
 /**
  * Utility class for escape operations
@@ -10,11 +9,9 @@ public class EscapeUtils {
     /**
      * Escape csv and excel problematic characters in the input string
      * @param valueToEscape The input string to escape
-     * @return A string containing the csv and excel problematic characters escaped
+     * @return A string containing the csv escaped value
      * */
     public String escapeForCsv(String valueToEscape) {
-        valueToEscape = RegExUtils.replaceAll(valueToEscape, "=", "'='");
-        valueToEscape = RegExUtils.replaceAll(valueToEscape, "-", "'-'");
         return StringEscapeUtils.escapeCsv(valueToEscape);
     }
 }
