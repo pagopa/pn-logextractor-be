69631856f170ea31ceefecf2a00897bddd9fdfcf
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-11-22 16:44:40 +0100

    code smells fixes

--- src/test/java/it/gov/pagopa/logextractor/util/ResponseConstructorTest.java
+++ src/test/java/it/gov/pagopa/logextractor/util/ResponseConstructorTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ResponseConstructorTest {
+class ResponseConstructorTest {
 
     @Test
     @DisplayName("Create zip archive with notification's related files")
