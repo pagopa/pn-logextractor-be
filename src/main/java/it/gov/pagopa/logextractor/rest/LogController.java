d0071503adad0a3ebbe621d8803eec042112edf1
Madlen Sarkisian <madlen.sarkisian@dxc.com>
2023-03-23 11:34:08 +0200

    Replaced with new headers

--- src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
+++ src/main/java/it/gov/pagopa/logextractor/rest/LogController.java
@@ -2,6 +2,9 @@ package it.gov.pagopa.logextractor.rest;
 
 import it.gov.pagopa.logextractor.pn_logextractor_be.api.LogsApi;
 import it.gov.pagopa.logextractor.pn_logextractor_be.model.*;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,12 +14,13 @@ import it.gov.pagopa.logextractor.service.LogService;
 @RestController
 public class LogController implements LogsApi {
 
+
 	@Autowired
 	LogService logService;
 
 	@Override
-	public ResponseEntity<BaseResponseDto> personActivityLogs(@RequestHeader(value="x-pagopa-uid", required=true) String xPagopaUid,
-   		 @RequestHeader(value="x-pagopa-cx-type", required=true) String xPagopaCxType, PersonLogsRequestDto personLogsRequestDto) throws Exception {
+	public ResponseEntity<BaseResponseDto> personActivityLogs(@RequestHeader(value="x-pagopa-pn-uid", required=true) String xPagopaUid,
+   		 @RequestHeader(value="x-pagopa-pn-cx-type", required=true) String xPagopaCxType, PersonLogsRequestDto personLogsRequestDto) throws Exception {
 		if (Boolean.TRUE.equals(personLogsRequestDto.getDeanonimization())) {
 			return ResponseEntity.ok().body(logService.getDeanonimizedPersonLogs(personLogsRequestDto,
 					xPagopaUid, xPagopaCxType));
@@ -26,29 +30,29 @@ public class LogController implements LogsApi {
 	}
 
 	@Override
-	public ResponseEntity<BaseResponseDto> notificationInfoLogs(@RequestHeader(value="x-pagopa-uid", required=true) String xPagopaUid,
-	   		 @RequestHeader(value="x-pagopa-cx-type", required=true) String xPagopaCxType, NotificationInfoRequestDto notificationInfoRequestDto) throws Exception {
+	public ResponseEntity<BaseResponseDto> notificationInfoLogs(@RequestHeader(value="x-pagopa-pn-uid", required=true) String xPagopaUid,
+	   		 @RequestHeader(value="x-pagopa-pn-cx-type", required=true) String xPagopaCxType, NotificationInfoRequestDto notificationInfoRequestDto) throws Exception {
 		return ResponseEntity.ok().body(logService.getNotificationInfoLogs(notificationInfoRequestDto,
 				xPagopaUid, xPagopaCxType));
 	}
 
 	@Override
-	public ResponseEntity<BaseResponseDto> notificationsInMonth(@RequestHeader(value="x-pagopa-uid", required=true) String xPagopaUid,
-	   		 @RequestHeader(value="x-pagopa-cx-type", required=true) String xPagopaCxType, MonthlyNotificationsRequestDto monthlyNotificationsRequestDto) throws Exception {
+	public ResponseEntity<BaseResponseDto> notificationsInMonth(@RequestHeader(value="x-pagopa-pn-uid", required=true) String xPagopaUid,
+	   		 @RequestHeader(value="x-pagopa-pn-cx-type", required=true) String xPagopaCxType, MonthlyNotificationsRequestDto monthlyNotificationsRequestDto) throws Exception {
 		return ResponseEntity.ok().body(logService.getMonthlyNotifications(monthlyNotificationsRequestDto,
 				xPagopaUid, xPagopaCxType));
 	}
 
 	@Override
-	public ResponseEntity<BaseResponseDto> processLogs(@RequestHeader(value="x-pagopa-uid", required=true) String xPagopaUid,
-	   		 @RequestHeader(value="x-pagopa-cx-type", required=true) String xPagopaCxType, TraceIdLogsRequestDto traceIdLogsRequestDto) throws Exception {
+	public ResponseEntity<BaseResponseDto> processLogs(@RequestHeader(value="x-pagopa-pn-uid", required=true) String xPagopaUid,
+	   		 @RequestHeader(value="x-pagopa-pn-cx-type", required=true) String xPagopaCxType, TraceIdLogsRequestDto traceIdLogsRequestDto) throws Exception {
 		return ResponseEntity.ok().body(logService.getTraceIdLogs(traceIdLogsRequestDto,
 				xPagopaUid, xPagopaCxType));
 	}
 	
 	@Override
-	public ResponseEntity<BaseResponseDto> sessionLogs(@RequestHeader(value="x-pagopa-uid", required=true) String xPagopaUid,
-	   		 @RequestHeader(value="x-pagopa-cx-type", required=true) String xPagopaCxType, SessionLogsRequestDto sessionLogsRequestDto) throws Exception {
+	public ResponseEntity<BaseResponseDto> sessionLogs(@RequestHeader(value="x-pagopa-pn-uid", required=true) String xPagopaUid,
+	   		 @RequestHeader(value="x-pagopa-pn-cx-type", required=true) String xPagopaCxType, SessionLogsRequestDto sessionLogsRequestDto) throws Exception {
 		if (Boolean.TRUE.equals(sessionLogsRequestDto.getDeanonimization())) {
 			return ResponseEntity.ok(logService.getDeanonimizedSessionLogs(sessionLogsRequestDto,
 					xPagopaUid, xPagopaCxType));
