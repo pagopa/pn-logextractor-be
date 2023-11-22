01a274d9fa4cf14024a060420599a16f7a53c405
Ivan Rosina <ivan.rosina@cinetix-team.it>
2023-11-22 15:49:07 +0100

    8790

---
+++ src/main/java/it/gov/pagopa/logextractor/config/ClientAspectLoggingActivation.java
@@ -0,0 +1,8 @@
+package it.gov.pagopa.logextractor.config;
+
+import org.springframework.stereotype.Component;
+import it.pagopa.pn.commons.utils.ClientAspectLogging;
+
+@Component
+public class ClientAspectLoggingActivation extends ClientAspectLogging {
+}
