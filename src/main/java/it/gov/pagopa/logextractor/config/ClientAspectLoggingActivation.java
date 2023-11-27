f263234847b146d045894bbf9badc10754cedcef
Ivan Rosina <ivan.rosina@cinetix-team.it>
2023-11-27 10:29:14 +0100

    fix build

--- src/main/java/it/gov/pagopa/logextractor/config/ClientAspectLoggingActivation.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.logextractor.config;
-
-import org.springframework.stereotype.Component;
-import it.pagopa.pn.commons.utils.ClientAspectLogging;
-
-@Component
-public class ClientAspectLoggingActivation extends ClientAspectLogging {
-}
