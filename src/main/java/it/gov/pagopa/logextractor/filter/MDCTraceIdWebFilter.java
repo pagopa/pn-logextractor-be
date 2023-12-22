d3ccf19ea7c9ff62d70dab87456945f705af6cf2
Ivan Rosina <ivan.rosina@cinetix-team.it>
2023-12-22 12:39:30 +0100

    test

--- src/main/java/it/gov/pagopa/logextractor/filter/MDCTraceIdWebFilter.java
+++ src/main/java/it/gov/pagopa/logextractor/filter/MDCTraceIdWebFilter.java
@@ -1,14 +1,36 @@
 package it.gov.pagopa.logextractor.filter;
 
-import org.springframework.stereotype.Component;
+import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+
+import it.gov.pagopa.logextractor.service.LogServiceImpl;
 import it.pagopa.pn.commons.log.MDCWebFilter;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 /**
  * WebFilter that puts in the MDC log map a unique identifier for incoming requests.
  */
 @Component
+@Slf4j
 public class MDCTraceIdWebFilter extends MDCWebFilter {
+	
+	
+	@Override
+    public @NotNull Mono<Void> filter(@NotNull ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+		log.info("filtro per intercettare trace_id");
+		HttpHeaders requestHeaders = serverWebExchange.getRequest().getHeaders();
+		List<String> traceIdHeaders = requestHeaders.get("X-Amzn-Trace-Id");
+		for (String t: traceIdHeaders) {
+			log.info("X-Amzn-Trace-Id:{}",t);
+		}
+		return super.filter(serverWebExchange, webFilterChain);
+	}
 
 
 }
